c6e01a3516123549406ff8408b446b47ece6d966
sui18001 <sui18001@student.mdh.se>
2018-09-23 15:45:37 +0200

    finalize lab assignment 2 - fix

--- frontend/html/article.php
+++ frontend/html/article.php
@@ -19,16 +19,16 @@ if( $S->isLoggedIn('frontend') === true ) { $isLogin = true; }
 			<div id="main_container" class="main-container">
 				<div class="page-title">article page</div>
 				<div id="article" name="article">
-					<?php if(isset($_GET['id']) && intval($_GET['id'])) { 
-						$articleId = intval($_GET['id']);
-						$isLoaded = true;
-						$ART = new Article();
-						$return = $ART->loadArticleById($articleId);
-						if($return['status'] === false) {
-							echo "<div class=\"article-error\">".$return['msg']."</div>";
-							$isLoaded = false;
-						} 
-					}
+					<?php $isLoaded = true;
+						if(isset($_GET['id']) && intval($_GET['id'])) { 
+							$articleId = intval($_GET['id']);
+							$ART = new Article();
+							$return = $ART->loadArticleById($articleId);
+							if($return['status'] === false) {
+								echo "<div class=\"article-error\">".$return['msg']."</div>";
+								$isLoaded = false;
+							} 
+						} else { echo "<div class=\"article-error\">there are no article id</div>"; $isLoaded = false; }
 					?>
 					<div id="article_title" name="article_title" class="article-title"><h1><?=($isLoaded)?($ART->getArticleTitle()):('')?></h1></div>
 					<div id="article_subtitle" name="article_subtitle" class="article-subtitle"><h2><?=($isLoaded)?($ART->getArticleSubtitle()):('')?></h2></div>
